df42ddf48510acc85ca77f5dae840a0c85d6eb13
Doug Rathbone <dougrathbone@gmail.com>
2026-05-30 20:28:36 +1000

    feat: electron main entry, window with secure webPreferences, vite config

---
+++ src/main/index.ts
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { registerIpc } from './ipc';
+
+let win: BrowserWindow | null = null;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1100,
+    height: 760,
+    webPreferences: {
+      preload: path.join(__dirname, '../preload/index.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+  if (process.env.ELECTRON_RENDERER_URL) {
+    win.loadURL(process.env.ELECTRON_RENDERER_URL);
+  } else {
+    win.loadFile(path.join(__dirname, '../renderer/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  registerIpc(() => win);
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
